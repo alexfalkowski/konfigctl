d9302433f7707a17f0008326235ae72e39d4498c
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-18 09:00:29 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.376.0
    https://github.com/alexfalkowski/go-service/pull/1194

--- cmd/secrets.go
+++ cmd/secrets.go
@@ -1,19 +1,18 @@
 package cmd
 
 import (
-	"github.com/alexfalkowski/go-service/compress"
-	"github.com/alexfalkowski/go-service/encoding"
-	"github.com/alexfalkowski/go-service/sync"
+	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/konfigctl/client"
 	"github.com/alexfalkowski/konfigctl/cmd/secrets"
 	"github.com/alexfalkowski/konfigctl/config"
+	"github.com/alexfalkowski/konfigctl/token"
 	"go.uber.org/fx"
 )
 
 // SecretsOptions for cmd.
 var SecretsOptions = []fx.Option{
-	sync.Module, compress.Module, encoding.Module,
+	module.Module, token.Module,
 	telemetry.Module, client.Module,
 	secrets.Module, config.Module, Module,
 }
