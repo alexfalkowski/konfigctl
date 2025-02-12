772255440c831cf881b0150f342585acb33aafac
Alex Falkowski <alexrfalkowski@gmail.com>
2025-02-12 21:27:13 +0100

    feat(cmd): move to use register

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -1,19 +1,21 @@
 package cmd
 
 import (
+	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/konfigctl/internal/client"
 	"github.com/alexfalkowski/konfigctl/internal/cmd/config"
 	kc "github.com/alexfalkowski/konfigctl/internal/config"
 	"github.com/alexfalkowski/konfigctl/internal/token"
-	"go.uber.org/fx"
 )
 
-// ConfigOptions for cmd.
-var ConfigOptions = []fx.Option{
-	module.Module, token.Module,
-	telemetry.Module, config.Module,
-	client.Module, kc.Module,
-	config.Module, Module,
+// RegisterConfig for cmd.
+func RegisterConfig(command *cmd.Command) {
+	client := command.AddClient("config", "Get Config.", module.Module, token.Module,
+		telemetry.Module, config.Module,
+		client.Module, kc.Module,
+		config.Module, Module,
+	)
+	command.RegisterOutput(client, "env:KONFIG_APP_CONFIG_FILE")
 }
